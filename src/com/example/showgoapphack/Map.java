c613d625d8052b003696525dbda77d890171f083
Drew Vance <drew@drewvance.com>
2013-11-03 00:24:06 -0700

    Added code to display bands/play buttons

--- src/com/example/showgoapphack/Map.java
+++ src/com/example/showgoapphack/Map.java
@@ -1,15 +1,23 @@
 package com.example.showgoapphack;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.EditText;
 
 public class Map extends Activity {
-
+	public final static String EVENT_ID = "EVENT_ID";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map);
+		
+		// TODO: replace with map;
+		Intent intent = new Intent(this, Lineup.class);
+    	intent.putExtra(EVENT_ID, "1");
+    	startActivity(intent);
 	}
 
 	@Override
